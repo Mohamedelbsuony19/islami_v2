0e77cce6e21b1ecd46c818780aae1fde64fff1d5
PC <Mohamedelbsuony19>
2022-12-15 17:56:32 +0200

    created home screen added quraan tab created quraan details

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,10 +1,74 @@
 import 'package:flutter/material.dart';
 
-class homeLayout extends StatelessWidget {
+import '../modules/hadeth/hadeth_screen.dart';
+import '../modules/quraan/quran_screen.dart';
+import '../modules/radio/radio_screen.dart';
+import '../modules/sebha/sebha_screen.dart';
+
+class homeLayout extends StatefulWidget {
   static const String routrName = 'home';
 
   @override
+  State<homeLayout> createState() => _homeLayoutState();
+}
+
+class _homeLayoutState extends State<homeLayout> {
+  late int curIndex = 0;
+  late List screens = [
+    quraanScreen(),
+    hadethScreen(),
+    sebhaScreen(),
+    radioScreen()
+  ];
+
+// assetes/images/bg_light.png
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Stack(
+      children: [
+        Image.asset(
+          'assetes/images/bg_light.png',
+          width: double.infinity,
+          height: double.infinity,
+          fit: BoxFit.fill,
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'ISLAMI',
+              style: Theme.of(context).textTheme.headline1,
+            ),
+          ),
+          body: screens[curIndex],
+          bottomNavigationBar: Theme(
+            data: Theme.of(context)
+                .copyWith(canvasColor: Theme.of(context).primaryColor),
+            child: BottomNavigationBar(
+              currentIndex: curIndex,
+              onTap: (index) {
+                curIndex = index;
+                setState(() {});
+              },
+              items: [
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assetes/images/ic_qraan.png')),
+                    label: 'quraan'),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assetes/images/ic_hadeth.png')),
+                    label: 'hadeth'),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assetes/images/ic_sebha.png')),
+                    label: 'sebha'),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(
+                      AssetImage('assetes/images/ic_radio.png'),
+                    ),
+                    label: 'radio'),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:islami_app/layout/home_layout.dart';
+import 'package:islami_app/modules/quraan/soura_details/soura_details.dart';
+import 'package:islami_app/shared/style/myTheme.dart';
 
 void main() {
   runApp(myApp());
@@ -9,8 +11,12 @@ class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {homeLayout.routrName: (buildContext) => homeLayout()},
+      routes: {
+        homeLayout.routrName: (buildContext) => homeLayout(),
+        souraDetails.route_Name: (buildContext) => souraDetails()
+      },
       initialRoute: homeLayout.routrName,
+      theme: MyTheme.lightTheme,
     );
   }
 }
